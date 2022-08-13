9abd9e309a2603a5180c25463d9ae54b36727ba1
DefinitelyNotACactus <galvao2010@icloud.com>
2022-08-13 09:49:46 -0300

    Add Yield Curve Graph

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from dash import Dash, dcc, html, Input, Output, State, no_update, callback_context
 
+import dash_bootstrap_components as dbc
+
 import pandas as pd
 import numpy as np
 
@@ -63,22 +65,58 @@ def plot_tree_map(data):
 	fig.data[0].customdata = fig.data[0].marker.colors
 	fig.data[0].texttemplate = "<b>%{label}</b><br>%{customdata:.2f}%<br>"
 	fig.update_layout(height=720)
+
+	return fig
+
+def plot_yield_curve(ntnf, previous=21):
+	current_date, previous_date = tr.NTNF_LAST_UPDATE, str(tr.NTNF_DICT[tr.NTNF.sheet_names[0][-2:]].index[-previous]).split(' ')[0]
+	x, y, y_previous = np.zeros(len(ntnf)), np.zeros(len(ntnf)), np.zeros(len(ntnf))
+	for i, year in enumerate(ntnf.keys()):
+		x[i] = int('20{}'.format(year))
+		y[i] = float(ntnf[year].iloc[-1]['Taxa Compra Manhã']) * 100
+		y_previous[i] = float(ntnf[year].iloc[-previous]['Taxa Compra Manhã']) * 100
+
+	fig = go.Figure()
+	fig.update_layout(margin=dict(l=16, r=16, t=16, b=16), 
+		xaxis=dict(title='Vencimento', tickmode='array', tickvals=x, showgrid=False, zeroline=False), 
+		yaxis=dict(title='Taxa', showticklabels=False, showgrid=False, zeroline=False),
+		plot_bgcolor="rgba(203, 203, 212, .3)",
+        paper_bgcolor="rgba(0,0,0,0)",
+        font_family="DIN Alternate",
+        legend=dict(orientation="h", yanchor="bottom", y=1, xanchor="right", x=1)
+        )
+
+	fig.add_trace(go.Scatter(x=x, y=y, name='Curva de Juros em {}'.format(current_date), line_shape='spline',
+			mode='lines+text', text=['{:.2f}%'.format(value) for value in y],  textposition='top center'))
+	fig.add_trace(go.Scatter(x=x, y=y_previous, name='Curva de Juros em {}'.format(previous_date), line_shape='spline',
+				mode='lines+text', text=['{:.2f}%'.format(value) for value in y_previous],  textposition='top center'))
 	return fig
 
 app.layout = html.Div(children=[
-		html.Div(children=[
-			html.H4('Tesouro Selic'),
-			html.P('Data referência: {}'.format(tr.LFT_LAST_UPDATE)),
-			treasury_indicator(tr.LFT_DICT),
-			html.H4('Tesouro IPCA+'),
-			html.P('Data referência: {}'.format(tr.NTNB_LAST_UPDATE)),
-			treasury_indicator(tr.NTNB_DICT),
-			html.H4('Tesouro Pré-fixado'),
-			html.P('Data referência: {}'.format(tr.NTNF_LAST_UPDATE)),
-			treasury_indicator(tr.NTNF_DICT),
-		], className='column'),
+	html.Header(children=[
+			html.H1('FII Watch'),
+		]),
+	html.Div(children=[
+			html.Div(children=[
+				html.H4('Tesouro Selic'),
+				html.P('Data referência: {}'.format(tr.LFT_LAST_UPDATE)),
+				treasury_indicator(tr.LFT_DICT),
+				html.H4('Tesouro IPCA+'),
+				html.P('Data referência: {}'.format(tr.NTNB_LAST_UPDATE)),
+				treasury_indicator(tr.NTNB_DICT),
+				html.H4('Tesouro Pré-fixado'),
+				html.P('Data referência: {}'.format(tr.NTNF_LAST_UPDATE)),
+				treasury_indicator(tr.NTNF_DICT),
+				html.H4('Curva de Juros'),
+				html.Div(children=[
+					dcc.Graph(id='yield-curve', config={'displayModeBar': False}, figure=plot_yield_curve(tr.NTNF_DICT)),
+					], className='column', style={"margin-left": "0", "position": "relative"}),
+			], className='column', id='treasury-div'),
+		], className='main-div'),
 		#dcc.Graph(id="scatter", config={"displayModeBar": False}, figure=plot_tree_map(ifix))
-	], className='main-div')
+	html.Footer(children=[
+			html.H4('Todas as informações apresentadas por este aplicativo possuem carater informativo e provêm de fontes públicas. O FII Watch não se responsabiliza pelas decisões e caminhos tomados pelo usuário a partir da análise das informações apresentadas.'),
+	]),])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
