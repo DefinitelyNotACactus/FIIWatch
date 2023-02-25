5cf43b65d1c37ff0bff725f7ddd465cb3620451e
DefinitelyNotACactus <galvao2010@icloud.com>
2023-02-25 14:54:33 -0300

    Treasury visual update

--- treasury.py
+++ treasury.py
@@ -12,20 +12,21 @@ def parse_sheet(table, sheet):
     return df
 
 # IPCA + (Juros Semestrais)
-NTNB = pd.ExcelFile('https://cdn.tesouro.gov.br/sistemas-internos/apex/producao/sistemas/sistd/2022/NTN-B_2022.xls')
+NTNB = pd.ExcelFile('https://cdn.tesouro.gov.br/sistemas-internos/apex/producao/sistemas/sistd/2023/NTN-B_2023.xls')
 NTNB_DICT = {name[-2:]: parse_sheet(NTNB, name) for name in NTNB.sheet_names}
 NTNB_LAST_UPDATE = str(NTNB_DICT[NTNB.sheet_names[0][-2:]].index[-1]).split(' ')[0]
 # IPCA +
-#NTNB_PRINCIPAL = pd.ExcelFile('https://cdn.tesouro.gov.br/sistemas-internos/apex/producao/sistemas/sistd/2022/NTN-B_Principal_2022.xls')
+#NTNB_PRINCIPAL = pd.ExcelFile('https://cdn.tesouro.gov.br/sistemas-internos/apex/producao/sistemas/sistd/2023/NTN-B_Principal_2023.xls')
 # IGPM +
-#NTNC = pd.ExcelFile('https://cdn.tesouro.gov.br/sistemas-internos/apex/producao/sistemas/sistd/2022/NTN-C_2022.xls')
+#NTNC = pd.ExcelFile('https://cdn.tesouro.gov.br/sistemas-internos/apex/producao/sistemas/sistd/2023/NTN-C_2023.xls')
 # Pré-fixado (Juros Semestrais)
-NTNF = pd.ExcelFile('https://cdn.tesouro.gov.br/sistemas-internos/apex/producao/sistemas/sistd/2022/NTN-F_2022.xls')
+NTNF = pd.ExcelFile('https://cdn.tesouro.gov.br/sistemas-internos/apex/producao/sistemas/sistd/2023/NTN-F_2023.xls')
 NTNF_DICT = {name[-2:]: parse_sheet(NTNF, name) for name in NTNF.sheet_names}
 NTNF_LAST_UPDATE = str(NTNF_DICT[NTNF.sheet_names[0][-2:]].index[-1]).split(' ')[0]
 # Tesouro SELIC
-LFT = pd.ExcelFile('https://cdn.tesouro.gov.br/sistemas-internos/apex/producao/sistemas/sistd/2022/LFT_2022.xls')
+LFT = pd.ExcelFile('https://cdn.tesouro.gov.br/sistemas-internos/apex/producao/sistemas/sistd/2023/LFT_2023.xls')
+
 LFT_DICT = {name[-2:]: parse_sheet(LFT, name) for name in LFT.sheet_names}
 LFT_LAST_UPDATE = str(LFT_DICT[LFT.sheet_names[0][-2:]].index[-1]).split(' ')[0]
 # Pré-fixado
-#LTN = pd.ExcelFile('https://cdn.tesouro.gov.br/sistemas-internos/apex/producao/sistemas/sistd/2022/LTN_2022.xls')
+#LTN = pd.ExcelFile('https://cdn.tesouro.gov.br/sistemas-internos/apex/producao/sistemas/sistd/2023/LTN_2023.xls')
