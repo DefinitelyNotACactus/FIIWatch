94cb46b4d30fb4501a4a7aa7b34365d31463b377
DefinitelyNotACactus <galvao2010@icloud.com>
2023-08-02 21:07:16 -0300

    Add delta function

--- util.py
+++ util.py
@@ -23,3 +23,11 @@ def indicator_delta(current, previous, kind_delta='relative', prefix='', suffix=
 		return html.P([
 			html.Span('{}{:.2f}{} = 0{}'.format(prefix, current, suffix, suffix_delta)),
 			])
+
+def price_delta(delta):
+	if delta > 0:
+		return html.P('▲ {:.2f}%'.format(delta), style={'color': 'green'})
+	elif delta < 0:
+		return html.P('▼ {:.2f}%'.format(delta), style={'color': 'red'})
+	else:
+		return html.P('= 0%'.format(delta))
