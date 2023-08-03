fff52a100d729508c056017d60ae49ec0180fc66
DefinitelyNotACactus <galvao2010@icloud.com>
2023-08-03 09:37:48 -0300

    Update index

--- app.py
+++
@@ -1,7 +0,0 @@
-from dash import Dash
-
-external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-app = Dash(__name__, external_stylesheets=external_stylesheets)
-app.title = "FII Watch"
-
-server = app.server
--- index.py
+++ index.py
@@ -1,33 +1,10 @@
-from dash import Dash, dcc, html, Input, Output, State, no_update, callback_context
+from dash import Dash, html, register_page, page_registry, page_container
 
-from app import app
-from pages import fii_dashboard, treasury_dashboard, info_dashboard
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+app = Dash(__name__, external_stylesheets=external_stylesheets, use_pages=True)
+app.title = "FII Watch"
 
-import fii
-
-info_tickers_path = [
-    f"/pages/info_dashboard/{symbol.lower()}" for symbol in fii.SYMBOLS
-]
-
-app.layout = html.Div(children=[
-        dcc.Location(id="url", refresh=False), 
-        html.Div(id="page-content"),
-    ]
-)
-
-@app.callback(Output("page-content", "children"), [Input("url", "pathname")])
-def display_page(pathname):
-    print(pathname)
-    if pathname == "/pages/treasury_dashboard":
-        return treasury_dashboard.layout
-    elif pathname == "/pages/fii_dashboard":
-        return fii_dashboard.layout
-    elif pathname == "/pages/info_dashboard":
-        return info_dashboard.get_layout()
-    elif pathname in info_tickers_path:
-        return info_dashboard.get_layout(pathname.split('/')[-1].upper())
-    else:
-        return treasury_dashboard.layout
+server = app.server
 
 if __name__ == "__main__":
     #app.run_server(host="0.0.0.0", debug=False)
