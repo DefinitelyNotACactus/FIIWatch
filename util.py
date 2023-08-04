a07f4893fd16e63343b1b6e8608d9007a2153a22
DefinitelyNotACactus <galvao2010@icloud.com>
2023-08-04 09:28:39 -0300

    Add earnings data

--- util.py
+++ util.py
@@ -1,5 +1,9 @@
 from dash import html
 
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+
 def get_delta(current, previous, kind='relative'):
 	if kind == 'relative': return current/previous - 1
 	else: return current-previous
@@ -24,10 +28,13 @@ def indicator_delta(current, previous, kind_delta='relative', prefix='', suffix=
 			html.Span('{}{:.2f}{} = 0{}'.format(prefix, current, suffix, suffix_delta)),
 			])
 
-def price_delta(delta):
+def price_delta(delta, H3=False):
 	if delta > 0:
-		return html.P('▲ {:.2f}%'.format(delta), style={'color': 'green'})
+		if H3 is False: return html.P('▲ {:.2f}%'.format(delta), style={'color': 'green'})
+		else: return html.H3('▲ {:.2f}%'.format(delta), style={'color': 'green'})
 	elif delta < 0:
-		return html.P('▼ {:.2f}%'.format(delta), style={'color': 'red'})
+		if H3 is False: return html.P('▼ {:.2f}%'.format(delta), style={'color': 'red'})
+		else: return html.H3('▼ {:.2f}%'.format(delta), style={'color': 'red'})
 	else:
-		return html.P('= 0%'.format(delta))
+		if H3 is False: return html.P('= 0%'.format(delta))
+		else: return html.H3('= 0%'.format(delta))
